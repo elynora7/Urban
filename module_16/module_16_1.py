f9ddb3f173d587d43171cb7b616802aeb78e8786
Helena Orlova <elynora7wah@gmail.com>
2025-01-02 19:03:10 +0300

    module_16

---
+++ module_16/module_16_1.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+# uvicorn module_16_1:app --reload
+app = FastAPI()
+
+
+@app.get('/')
+async def index() -> dict:
+    return {'message': 'Главная страница'}
+
+
+@app.get('/user/admin')
+async def get_admin() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+
+@app.get('/user/{user_id}')
+async def get_user(user_id: int) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+
+@app.get('/user')
+async def get_user_info(username: str, age: int) -> dict:
+    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
