7b6ef044e6715d39ef4771652b7d3ed314bb0909
Helena Orlova <elynora7wah@gmail.com>
2025-01-03 21:15:55 +0300

    module_17

--- module_17/fastApiProject/app/routers/user.py
+++ module_17/fastApiProject/app/routers/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from app.backend.db_depends import get_db
 from typing import Annotated
-from app.models import User
+from app.models import User, Task
 from app.schemas import CreateUser, UpdateUser
 from sqlalchemy import insert, select, update, delete
 from slugify import slugify
@@ -25,9 +25,20 @@ async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
         detail="User was not found"
     )
 
+@router.get("/user_id/tasks")
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    if (user := db.scalars(select(User).where(User.id == user_id)).first()):
+        tasks = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+        return tasks
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User was not found"
+    )
+
 
 @router.post("/create")
-async def create_user(db: Annotated[Session, Depends(get_db)], create_user: CreateUser):
+async def create_user(db: Annotated[Session, Depends(get_db)],
+                      create_user: CreateUser):
     db.execute(insert(User).values(username=create_user.username,
                                    firstname=create_user.firstname,
                                    lastname=create_user.lastname,
@@ -41,7 +52,8 @@ async def create_user(db: Annotated[Session, Depends(get_db)], create_user: Crea
 
 
 @router.put("/update")
-async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int, update_user: UpdateUser):
+async def update_user(db: Annotated[Session, Depends(get_db)],
+                      user_id: int, update_user: UpdateUser):
     user = db.scalars(select(User).where(User.id == user_id)).first()
     if user is None:
         raise HTTPException(
@@ -68,6 +80,7 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
             status_code=status.HTTP_404_NOT_FOUND,
             detail="User was not found"
         )
+    db.execute(delete(Task).where(Task.user_id == user_id))
     db.execute(delete(User).where(User.id == user_id))
     db.commit()
     return {
