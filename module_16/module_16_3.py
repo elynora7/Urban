195d6c7de7d682d81f8ff6fb02ac8bb07066cf29
Helena Orlova <elynora7wah@gmail.com>
2025-01-12 18:28:43 +0300

    module_16

--- module_16/module_16_3.py
+++ module_16/module_16_3.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 from typing import Annotated
 
 app = FastAPI()
@@ -25,11 +25,16 @@ async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='En
                       username: Annotated[str, Path(min_length=5, max_length=20,
                                                     description='Enter username', example='UrbanUser')],
                       age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]) -> str:
-    users[str(user_id)] = f'Имя: {username}, возраст: {age}'
-    return f'User {user_id} is updated'
+    if str(user_id) in users:
+        users[str(user_id)] = f'Имя: {username}, возраст: {age}'
+        return f'User {user_id} is updated'
+
+    raise HTTPException(status_code=404, detail='User was not found')
 
 
 @app.delete('/user/{user_id}')
 async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=1)]) -> str:
-    del users[str(user_id)]
-    return f'User {user_id} is deleted'
+    if str(user_id) in users:
+        del users[str(user_id)]
+        return f'User {user_id} is deleted'
+    raise HTTPException(status_code=404, detail='User was not found')
